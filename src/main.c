026c55254a4e4a52606fb58e3efa8fd6bbc17599
Mehdi Ben Hadj Khelifa <mehdi.bhk13@gmail.com>
2023-09-21 17:32:07 +0100

    added registers,condition flags and opcodes

--- src/main.c
+++ src/main.c
@@ -1,7 +1,58 @@
 #include <stdio.h>
 
+
+//defining memory for the LC3 VM
 #define MEMORY_MAX (1 << 16)
 uint16_t memory[MEMORY_MAX];
+
+
+//defining CPU registers for the LC3 VM
+enum
+{
+    R_R0 = 0,
+    R_R1,
+    R_R2,
+    R_R3,
+    R_R4,
+    R_R5,
+    R_R6,
+    R_R7,
+    R_PC, /* program counter */
+    R_COND,
+    R_COUNT
+};
+uint16_t reg[R_COUNT];
+
+//condition flags for the R_COND register
+enum
+{
+    FL_POS = 1 << 0, /* P */
+    FL_ZRO = 1 << 1, /* Z */
+    FL_NEG = 1 << 2, /* N */
+};
+
+//defining opcodes for the LC3 VM
+//4 left bits are instructions,12 bits for task parametres
+enum
+{
+    OP_BR = 0, /* branch */
+    OP_ADD,    /* add  */
+    OP_LD,     /* load */
+    OP_ST,     /* store */
+    OP_JSR,    /* jump register */
+    OP_AND,    /* bitwise and */
+    OP_LDR,    /* load register */
+    OP_STR,    /* store register */
+    OP_RTI,    /* unused */
+    OP_NOT,    /* bitwise not */
+    OP_LDI,    /* load indirect */
+    OP_STI,    /* store indirect */
+    OP_JMP,    /* jump */
+    OP_RES,    /* reserved (unused) */
+    OP_LEA,    /* load effective address */
+    OP_TRAP    /* execute trap */
+}; 
+
 
 int main(int argc,char* argv[]){
 	printf("Hello World!\n");
